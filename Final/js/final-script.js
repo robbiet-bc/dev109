b89e92f85f5fb34d2300d99b707cb978423941d2
robbiet-bc <rob.taylor@bellevuecollege.edu>
2024-11-26 17:39:59 -0800

    refreshing final

--- Final/js/final-script.js
+++ Final/js/final-script.js
@@ -1,33 +1,87 @@
-// A list of images in my GitHub
-// JavaScript to populate the image grid
-const imagePaths = [
-  '../a2/images/black-bishop.png',
-  '../a2/images/gold-bishop.png',
-  '../a3/images/geometric-grey-background.avif',
-  '../m01/images/Agua-de-Horchata-2.jpg',
-  '../m01/images/Albondigas-Mexican-Meatballs-2.jpg',
-  '../m01/images/German-Apple-Tart.jpg',
-  '../m01/images/Mauritian-Chicken-Curry.jpg',
-  '../m01/images/Pan-Seared-Salmon-Close-up.jpg',
-  '../m01/images/Tallarin-Verde.jpg',
-  '../m04/images/forest-green.png',
-  '../m04/images/lite-green.png',
-  '../m05/images/bullseye-logo.png',
-  '../m05/images/teacher.png',
-  '../Final/images/grow-deep.png',
-  '../Final/images/soft-greens.png',
-  '../Final/images/taylor-made.png',
-  '../Final/images/taylor-office.png'
-];
+if (window.location.pathname.endsWith("validated.html")) {
+  document.getElementById("myContact").addEventListener("submit", validateForm);
+}
 
-// Select the grid container
-const gridContainer = document.getElementById("artifacts-grid");
+// fullName VALIDATION ////////////////////////////////////////////////////////
+function validateFullName() {
+    var fullName = document.getElementById("FullName").value;
+    if (!fullName || fullName.length > 50 || fullName == "") {
+        showError("FullName", "Name is required and cannot be greater than 50 characters");
+        return false;
+    }
 
-// Iterate over each image path
-imagePaths.forEach((path) => {
-  const imgElement = document.createElement("img"); // Create an image element
-  imgElement.src = path; // Set the image source
-  imgElement.alt = "Image" + path; // Set alt text for accessibility
-  imgElement.loading = "lazy"; // Use lazy loading for better performance
-  gridContainer.appendChild(imgElement); // Add image to the grid container
-});
+    // regular expression to match only alphanumeric characters and spaces
+    var re = /^[\w ]+$/;
+    // validation fails if the input doesn't match our regular expression
+    if (!re.test(fullName)) {
+        alert("Error: Input contains invalid characters: " + fullName);
+        showError("FullName", "fullName contains invalid Characters.");
+        return false;
+    }
+    // validation was success
+
+    return true;
+}
+// Email VALIDATION ////////////////////////////////////////////////////////////
+function validateEmail() {
+    var userEmail = document.getElementById("email").value;
+    var atpos = userEmail.indexOf("@");
+    var dotpos = userEmail.lastIndexOf(".");
+
+    if (!userEmail || atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= userEmail.length) {
+        showError("email", "Invalid email");
+        return false;
+    }
+    return true;
+}
+
+// subject VALIDATION /////////////////////////////////////////////////////////
+function validatesubject() {
+    var subject = document.getElementById("subject").value;
+
+    if (!subject || subject == "null" || subject == "" || subject.length <= 1) {
+        showError("subject", "Subject is required and should have context.");
+        return false;
+    }
+    return true;
+}
+
+// Display Errors //////////////////////////////////////////////////////////////
+function showError(element_id, message) {
+    var element = document.getElementById(element_id);
+    var error_div = document.createElement('div');
+    error_div.id = element_id + '_error';
+    error_div.className = 'error';
+    error_div.innerHTML = message;
+    element.parentNode.insertBefore(error_div, element.nextSibling);
+
+}
+
+// Part of eliminating each error message  /////////////////////////////////////
+function removeElementsByClass(rootElement, className) {
+    var elements = rootElement.getElementsByClassName(className);
+    while (elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+// Clear Errors on Submit  /////////////////////////////////////////////////////
+function removeErrorMessages() {
+    removeElementsByClass(document.getElementById('myContact'), 'error');
+}
+
+// ALL VALIDATION //////////////////////////////////////////////////////////////
+function validateForm(event) {
+    removeErrorMessages();
+    if (validateFullName()
+        // && validatefullName() // rt.rt testing purposes
+        && validateEmail()
+        && validatesubject()
+    ) {
+        return true;
+    }
+    else {
+        event.preventDefault();
+        return false;
+    }
+};
